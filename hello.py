decbea404084a269ebba80b8ff3a7a5f8984b2b7
Guilherme Maciel <>
2019-02-28 06:02:11 -0300

    Yeah, python é life

--- hello.py
+++ hello.py
@@ -2,16 +2,16 @@ msg = 'Hello world'
 print (msg)
 a = 45 
 b = 25.5552
-print ('mensagem topzera %d de vdd' %a)
+print ('Int marcador %d, funciona msm.' %a)
 print ('String marcador %s, funciona msm.' %msg)
 print ('Float marcador %.2f, funciona msm.' %b)
 
 a, b = b, a
 
-print ('mensagem topzera %d de vdd' %a)
+print ('Int marcador %d, funciona msm.' %a)
 print ('String marcador %s, funciona msm.' %msg)
 print ('Float marcador %.2f, funciona msm.' %b)
 
 nome = input ('Digite seu nome: ')
 
-print ('Bem vindo %s'%nome)
+print ('Bem vindo %s' %nome)
